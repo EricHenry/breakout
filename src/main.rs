b356fcc9fdeff5d8b4bcb447995ec4fd94ba66ec
Eric Correia <correia.eh@gmail.com>
2022-05-24 20:51:49 -0400

    First system and plugin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(HelloPlugin)
+        .run();
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+// First System
+fn hello_world() {
+    println!("hello world!");
+}
+
+// Startup system, a system that runs only once, before all other systems
+fn add_people(mut commands: Commands) {
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Elaina Proctor".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Renzo Hume".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Zayna Nieves".to_string()));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("Hello {}!", name.0);
+    }
+}
+
+// First Plugin
+pub struct HelloPlugin;
+
+/// Plugins group together systems.
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(add_people)
+            .add_system(hello_world)
+            .add_system(greet_people);
+    }
 }
